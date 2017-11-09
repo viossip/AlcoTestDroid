da1206fe4650efbc2a800fd553b0246711e4c85b
ilya221990 <30519754+ilya221990@users.noreply.github.com>
2017-11-09 13:24:50 +0200

    add authentication

--- app/src/main/java/ilya/vitaly/alcotestdroid/GameUI/LoginActivity.java
+++ app/src/main/java/ilya/vitaly/alcotestdroid/GameUI/LoginActivity.java
@@ -57,8 +57,8 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         // buttons ///////
 
-        loginEmail = (EditText) findViewById(R.id.email);
-        loginPass = (EditText) findViewById(R.id.password);
+        loginEmail = (EditText) findViewById(R.id.login_email);
+        loginPass = (EditText) findViewById(R.id.login_password);
         btnSignIn = (Button) findViewById(R.id.sign_in_button);
         btnSignOut = (Button) findViewById(R.id.sign_out_button);
         btnShowRegForm = (Button) findViewById(R.id.show_register_btn);
@@ -67,7 +67,7 @@ public class LoginActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         /// database ///
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        mFirebaseDatabase = database.getReference("Users");
 
 //        myRef.setValue("Hello, World!");
 
@@ -145,7 +145,9 @@ public class LoginActivity extends AppCompatActivity {
                                 // Sign in success, update UI with the signed-in user's information
                                 Log.d(TAG, "createUserWithEmail:success");
                                 FirebaseUser user = mAuth.getCurrentUser();
-                                addUserToDB(new User(Integer.parseInt(user.getUid()),user.getEmail(), userName));
+                               addUserToDB(new User(user.getUid(),user.getEmail(), userName), mFirebaseDatabase);
+                                User localUser = new User (user.getUid(),user.getEmail(), userName);
+//                                mFirebaseDatabase.child("users").child(localUser.getID()).setValue(user);
                                toastMessage("You have register " + user.getEmail());
                             } else {
                                 // If sign in fails, display a message to the user.
@@ -158,8 +160,8 @@ public class LoginActivity extends AppCompatActivity {
                     });
     }
 
-    private void addUserToDB(User user) {
-
+    private void addUserToDB(User user ,DatabaseReference myref) {
+        myref.child("ID").child(user.getID()).setValue(user);
     }
 
     private void checkAuth(String email, String pass) {
